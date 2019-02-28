e4fb925a86c80212948e1d4f762911e36d1b59a3
leila-alderman <leila.alderman@gmail.com>
2019-02-28 15:08:37 -0500

    Add full_title to test helper and add test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,8 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # allows tests to use full_title helper
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end

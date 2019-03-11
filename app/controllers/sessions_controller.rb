a53a1f02de43157324443adde8521e320c0a41a6
leila-alderman <leila.alderman@gmail.com>
2019-03-11 16:03:09 -0400

    Add log_in helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,8 @@ class SessionsController < ApplicationController
     # If that user exists and is authenticated with the correct password:
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
+      log_in user
+      redirect_to user
     else
       # Create an error message
       flash.now[:danger] = 'Invalid email/password combination'

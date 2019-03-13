4356fff164407eaef7156f3407df1b80c47c52dc
leila-alderman <leila.alderman@gmail.com>
2019-03-13 17:37:54 -0400

    Implement friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -44,6 +44,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+    assert session[:forwarding_url].nil?
   end
 
 end

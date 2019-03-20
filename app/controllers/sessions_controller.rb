6838b88aa0e3f21350671ee1d412fe85a39d43bd
leila-alderman <leila.alderman@gmail.com>
2019-03-20 16:56:21 -0400

    Require activation for logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,19 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     # If that user exists and is authenticated with the correct password:
     if @user && @user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      # If the user has activated his/her account:
+      if user.activated?
+        # Log the user in and redirect to the user's show page
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      # else, if the account has not been activated yet
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # Create an error message
       flash.now[:danger] = 'Invalid email/password combination'

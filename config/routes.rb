e017a202df52f4c5afbf15985473d0329a40a42a
leila-alderman <leila.alderman@gmail.com>
2019-03-21 17:49:18 -0400

    Add PasswordResets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
@@ -12,5 +13,6 @@ Rails.application.routes.draw do
   delete '/logout',   to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 end

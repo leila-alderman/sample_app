d22c5a8a98126f38afbfb66ed439f9cb2a34c677
leila-alderman <leila.alderman@gmail.com>
2019-03-24 14:24:20 -0400

    Add microposts controller tests
    Add create and destroy routes for microposts.
    Add microposts controller tests to ensure that create and destroy
    requests are redirected to the login_url when a user is not logged in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'static_pages#home'
   get '/help',        to: 'static_pages#help'
   get '/about',       to: 'static_pages#about'
@@ -14,5 +12,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-
+  resources :microposts, only: [:create, :destroy]
 end
